b3d5650e0cc4171c0d5b5cd30459fa0e816dcced
GitHub Action <action@github.com>
2024-12-04 19:47:27 +0000

    Update formula to version v0.0.1.6

--- Formula/heulpad.rb
+++ Formula/heulpad.rb
@@ -1,14 +1,14 @@
 class Heulpad < Formula
-  desc "Your CLI tool description here"
+  desc "Manage content from inside the terminal"
   homepage "https://github.com/grabbiel/heulpad"
-  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.1.5.tar.gz"
-  sha256 "3f83680a42737fa80a6b46f9c830c6f0dc73681fd5188b5385d63a090b9a9046"
+  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.1.6.tar.gz"
+  sha256 "862e1bac67ff4999cf00990ba7ff6039a9651d787b819ffd8a68e17a06a8de67"
   license "MIT"
 
   depends_on "cmake" => :build
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_BUILD_TYPE=Release", *std_cmake_args
     system "cmake", "--build", "build"
     bin.install "build/heulpad"
   end
