6bc1b0a52b13d899a5ca37a0dd46c351c7a8913c
GitHub Action <action@github.com>
2024-12-04 20:13:18 +0000

    Update formula to version v0.0.1.9.1

--- Formula/heulpad.rb
+++ Formula/heulpad.rb
@@ -1,8 +1,8 @@
 class Heulpad < Formula
   desc "Manage content from inside the terminal"
   homepage "https://github.com/grabbiel/heulpad"
-  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.1.8.tar.gz"
-  sha256 "44627741ddafc98e4597fe3dd849390f8606000226f52f912583c740220844ce"
+  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.1.9.1.tar.gz"
+  sha256 "7baecf43dbb16edb952d4a1231b5e39570c1c7a173f8a82220bef0a5fdd30971"
   license "MIT"
 
   depends_on "cmake" => :build
@@ -12,7 +12,7 @@ class Heulpad < Formula
     system "cmake", "--build", "build"
     bin.install "build/heulpad"
 
-    (File.join(Dir.home, ".heulpad")).mkpath
+    Pathname.new(File.join(Dir.home, ".heulpad")).mkpath
   end
 
   def post_install
@@ -30,6 +30,6 @@ class Heulpad < Formula
 
   test do
     system "#{bin}/heulpad", "--version"
-    assert_predicate File.join(Dir.home, ".heulpad"), :directory?
+    assert_predicate Pathname.new(File.join(Dir.home, ".heulpad")), :directory?
   end
 end
