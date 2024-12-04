fb752d18371a87f5612da8ad239327397e50dcab
GitHub Action <action@github.com>
2024-12-04 20:02:36 +0000

    Update formula to version v0.0.1.7

--- Formula/heulpad.rb
+++ Formula/heulpad.rb
@@ -1,8 +1,8 @@
 class Heulpad < Formula
   desc "Manage content from inside the terminal"
   homepage "https://github.com/grabbiel/heulpad"
-  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.1.6.tar.gz"
-  sha256 "862e1bac67ff4999cf00990ba7ff6039a9651d787b819ffd8a68e17a06a8de67"
+  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.1.7.tar.gz"
+  sha256 "3e997b0c52a685475e3ef1ea3c7a1cb0da3f15c5569fd7431a1b683316be7401"
   license "MIT"
 
   depends_on "cmake" => :build
@@ -11,9 +11,25 @@ class Heulpad < Formula
     system "cmake", "-S", ".", "-B", "build", "-DCMAKE_BUILD_TYPE=Release", *std_cmake_args
     system "cmake", "--build", "build"
     bin.install "build/heulpad"
+
+    (Dir.home/".heulpad").mkpath
+  end
+
+  def post_install
+    system "chmod", "700", File.expand_path("~/.heulpad")
+  end
+
+  def caveats
+    <<~EOS
+      User configuration datat is stored in ~/.heulpad
+      
+      To completely remove all data after uninstalling:
+        rm -rf ~/.heulpad
+    EOS
   end
 
   test do
     system "#{bin}/heulpad", "--version"
+    assert_predicate Dir.home/".heulpad", :directory?
   end
 end
