7e62ee05acbea647fde0949bc373daf555c55ab5
GitHub Action <action@github.com>
2024-12-04 20:21:30 +0000

    Update formula to version v0.idk

--- Formula/heulpad.rb
+++ Formula/heulpad.rb
@@ -1,8 +1,8 @@
 class Heulpad < Formula
   desc "Manage content from inside the terminal"
   homepage "https://github.com/grabbiel/heulpad"
-  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.9.2.tar.gz"
-  sha256 "99be96548228371a2de7a3cf4adbe411af6315ea3ba419eff8ada2069ab30e50"
+  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.idk.tar.gz"
+  sha256 "b0170ed3f26f98f5d11d268d34a0d23e4ed26753a8274bbe327f2c9e679b1f94"
   license "MIT"
 
   depends_on "cmake" => :build
@@ -11,11 +11,10 @@ class Heulpad < Formula
     system "cmake", "-S", ".", "-B", "build", "-DCMAKE_BUILD_TYPE=Release", *std_cmake_args
     system "cmake", "--build", "build"
     bin.install "build/heulpad"
-
-    Pathname.new(File.join(Dir.home, ".heulpad")).mkpath
   end
 
   def post_install
+    Pathname.new(File.join(Dir.home, ".heulpad")).mkpath
     system "chmod", "700", File.join(Dir.home, ".heulpad")
   end
 
