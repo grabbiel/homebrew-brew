c4d8de915b4c6e43df4d2d6c0b8184c45cd0ee6f
GitHub Action <action@github.com>
2024-12-04 23:13:54 +0000

    Update formula to version v0.0.15

--- Formula/heulpad.rb
+++ Formula/heulpad.rb
@@ -1,8 +1,8 @@
 class Heulpad < Formula
   desc "Manage content from inside the terminal"
   homepage "https://github.com/grabbiel/heulpad"
-  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.13.tar.gz"
-  sha256 "13d1ce8c59fca94022310878af6fdcbf4ee98fc77de78f77cc20326a1eb50ace"
+  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.15.tar.gz"
+  sha256 "b0dab0e13ce754256cbe7df65596d6b1706ddb37b0abb86a446c6a4e2a5cb2a6"
   license "MIT"
 
   depends_on "cmake" => :build
@@ -13,10 +13,8 @@ class Heulpad < Formula
     bin.install "build/heulpad"
 
     (etc/"heulpad").mkpath
-
     unless (etc/"heulpad/config").exist?
-      (etc/"heulpad/config").write<<~EOS
-        
+      (etc/"heulpad/config").write <<~EOS
         [settings]
         default_folder=/tmp
         auto_backup=true
