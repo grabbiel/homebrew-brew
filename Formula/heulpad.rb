b7aa8d467044319283ef39e58df6e692342bea92
GitHub Action <action@github.com>
2024-12-05 03:08:51 +0000

    Update formula to version v0.0.2

--- Formula/heulpad.rb
+++ Formula/heulpad.rb
@@ -1,8 +1,8 @@
 class Heulpad < Formula
   desc "Manage content from inside the terminal"
   homepage "https://github.com/grabbiel/heulpad"
-  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "406902f471d5c6ac3950c62f5a9d15e226b7a9f12c40e3e0f0e01b9084e77d3b"
+  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "a4eeedb62bd4c65a361247a0bd247b3fe329e47074237e1cb38b4189a36bcb7a"
   license "MIT"
 
   uses_from_macos "curl"
@@ -15,13 +15,23 @@ class Heulpad < Formula
 
     mv bin/"heulpad", bin/"heulpad.real"
 
+    # Create directories
     (etc/"heulpad").mkpath
     (var/"heulpad/plugins").mkpath
+    (share/"heulpad").mkpath
 
+    # Create plugins registry file
+    (share/"heulpad/plugins.list").write <<~EOS
+      cloud
+    EOS
+
+    # Create wrapper with environment variables 
     (bin/"heulpad").write_env_script bin/"heulpad.real",
       HEULPAD_CONFIG: "#{etc}/heulpad/config",
-      HEULPAD_PLUGINS: "{#var}/heulpad/plugins"
-      
+      HEULPAD_PLUGINS: "#{var}/heulpad/plugins"
+      HEULPAD_PLUGINS_REGISTRY: "#{share}/heulpad/plugins.list"
+     
+    # Create default config
     unless (etc/"heulpad/config").exist?
       (etc/"heulpad/config").write <<~EOS
         [settings]
@@ -35,6 +45,7 @@ class Heulpad < Formula
     <<~EOS
       Configuration: #{etc}/heulpad/config
       Plugins: #{var}/heulpad/plugins
+      Plugins Registry: #{share}/heulpad/plugins.list
     EOS
   end
 
@@ -42,5 +53,6 @@ class Heulpad < Formula
     system "#{bin}/heulpad", "--version"
     assert_predicate etc/"heulpad/config", :exist?
     assert_predicate var/"heulpad/plugins", :directory?
+    assert_predicate share/"heulpad/plugins.list", :exist?
   end
 end
