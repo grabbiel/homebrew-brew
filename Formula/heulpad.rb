e39279a8e519ab7ecd65a537e1f53870e187f3d9
GitHub Action <action@github.com>
2024-12-11 05:10:05 +0000

    Update formula to version v0.0.17

--- Formula/heulpad.rb
+++ Formula/heulpad.rb
@@ -1,23 +1,50 @@
 class Heulpad < Formula
   desc "Manage content from inside the terminal"
   homepage "https://github.com/grabbiel/heulpad"
-  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.16.tar.gz"
-  sha256 "88ed66b85d8ea7772e4aa34760f411f99a1e9b5e59f2136989470788172504f6"
+  url "https://github.com/grabbiel/heulpad/archive/refs/tags/v0.0.17.tar.gz"
+  sha256 "54827f1525aa7ca8777b239c07ed77621e1f2391eb6cdba9497086c7843ed175"
   license "MIT"
 
+  uses_from_macos "curl"
   depends_on "cmake" => :build
 
   def install
     system "cmake", "-S", ".", "-B", "build", "-DCMAKE_BUILD_TYPE=Release", *std_cmake_args
     system "cmake", "--build", "build"
+    
     bin.install "build/heulpad"
+    libexec.install Dir["build/src/commands/**/heulpad-*"]
+    man1.install Dir["man/*.1"]
+
+    chmod 0755, Dir[libexec/"heulpad-*"]
 
     mv bin/"heulpad", bin/"heulpad.real"
 
-    (bin/"heulpad").write_env_script bin/"heulpad.real",
-      HEULPAD_CONFIG: "#{etc}/heulpad/config"
-
+    # Create directories
     (etc/"heulpad").mkpath
+    (var/"heulpad/plugins").mkpath
+    (share/"heulpad").mkpath
+
+    # Create plugins registry file
+    (share/"heulpad/plugins.list").write <<~EOS
+      cloud
+    EOS
+
+    # Create Help file
+    (share/"heulpad/help.txt").write <<~EOS
+      enable: Enable and install plugin
+      disable: Disable an installed plugin
+      new: Initialize project in current directory
+    EOS
+
+    # Create wrapper with environment variables 
+    (bin/"heulpad").write_env_script bin/"heulpad.real",
+      HEULPAD_CONFIG: "#{etc}/heulpad/config",
+      HEULPAD_PLUGINS: "#{var}/heulpad/plugins",
+      HEULPAD_PLUGINS_REGISTRY: "#{share}/heulpad/plugins.list",
+      HEULPAD_LIBEXEC: libexec
+     
+    # Create default config
     unless (etc/"heulpad/config").exist?
       (etc/"heulpad/config").write <<~EOS
         [settings]
@@ -27,15 +54,26 @@ class Heulpad < Formula
     end
   end
 
+  def post_uninstall
+    rm_rf var/"heulpad/plugins"
+  end
+
   def caveats
     <<~EOS
-      Configuration files are installed to:
-      #{etc}/heulpad/config
+      Configuration: #{etc}/heulpad/config
+      Plugins: #{var}/heulpad/plugins
+      Plugins Registry: #{share}/heulpad/plugins.list
+
+      To completely remove all data after uninstalling:
+        rm -rf #{var}/heulpad
+        rm -rf #{etc}/heulpad
     EOS
   end
 
   test do
     system "#{bin}/heulpad", "--version"
     assert_predicate etc/"heulpad/config", :exist?
+    assert_predicate var/"heulpad/plugins", :directory?
+    assert_predicate share/"heulpad/plugins.list", :exist?
   end
 end
